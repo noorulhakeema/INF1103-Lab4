3ed42755f7bf4afd02c9ec878766296f3bfc5822
noorulhakeema <2604715@sit.singaporetech.edu.sg>
2026-09-25 22:05:31 +0800

    added load inventory and format as sample

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -26,21 +26,6 @@ def save_inventory(orders):
         file.write(item[0] + "," + item[1] + "," + item[2] + "\n")
     file.close()
     
-def get_valid_input():
-    global failed_attempts
-    while True:
-        new_stock = input("How many stocks to add, or type 'quit' to leave: ")
-
-        if new_stock.lower() == "quit":
-            return "quit"
-
-        if not new_stock.isdigit():
-            print("type a valid integer.")
-            failed_attempts +=1
-            continue
-
-        return int(new_stock)
-
 def process_delivery(current_total,new_value):
     new_total = current_total +new_value    
     return new_total
@@ -54,6 +39,30 @@ def generate_report(total_units, failed_attempts):
     print('Total deliveries processed:', total_units)
     print('number of failed entries:', failed_attempts)
 
+orders = load_inventory()
+
+print("Current Orders:\n")
+if len(orders) == 0:
+    print("No orders found in inventory.txt")
+else:
+    for item in orders:
+        print(item[0] + ", " + item[1] + ", " + item[2])
+    print()
+
+def get_valid_input():
+    global failed_attempts
+    while True:
+        product_name = input("How many stocks to add, or type 'quit' to leave: ")
+
+        if product_name.lower() == "quit":
+            return "quit"
+
+        if not product_name.isdigit():
+            print("type a valid integer.")
+            failed_attempts +=1
+            continue
+
+        return int(new_stock)
 
 while True:
     new_stock = get_valid_input()
